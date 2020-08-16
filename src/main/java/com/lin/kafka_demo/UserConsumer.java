1b3a5026ae15f22b696abfa47e1291419dc92ecd
BIIIing <1556629488@qq.com>
2020-08-16 17:41:08 +0800

    提交

---
+++ src/main/java/com/lin/kafka_demo/UserConsumer.java
@@ -0,0 +1,45 @@
+package com.lin.kafka_demo;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class UserConsumer {
+    private static final String brokerList = "192.168.18.128:9092";
+    private static final String topic = "lin";
+    private static final String groupId = "group.demo";
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 设置key序列化器
+        //properties.put("key.deserializer",
+        //        "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // 设置值序列化
+        //properties.put("value.deserializer",
+        //        "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, UserDeserializer.class.getName());
+
+        // 设置消费组
+        //properties.put("group.id", groupId);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        //设置集群地址
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        KafkaConsumer<String,UserDeserializer> kafkaConsumer = new KafkaConsumer<String, UserDeserializer>(properties);
+        kafkaConsumer.subscribe(Collections.singletonList(topic));
+        while (true){
+            ConsumerRecords<String,UserDeserializer> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+            if (records == null){
+                continue;
+            }
+            for (ConsumerRecord<String,UserDeserializer> record:records){
+                System.out.println(record.value());
+            }
+        }
+    }
+}
--- src/main/java/com/lin/kafka_demo/UserDeserializer.java
+++ src/main/java/com/lin/kafka_demo/UserDeserializer.java
@@ -1,5 +1,6 @@
 package com.lin.kafka_demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 
@@ -17,41 +18,14 @@ public class UserDeserializer implements Deserializer<User> {
 
     @Override
     public User deserialize(String s, byte[] data) {
-        Integer id;
-        int nameSize;
-        String name;
-        int sexSize;
-        String sex;
+        ObjectMapper mapper = new ObjectMapper();
+        User user = null;
         try {
-            if (null == data){
-                return null;
-            }
-            if (data.length < 12){
-                throw  new SerializationException("接收的数据的大小比预期的要短");
-            }
-            ByteBuffer buffer = ByteBuffer.wrap(data);
-
-            id = Integer.valueOf(buffer.getInt());
-
-            nameSize = buffer.getInt();
-            byte[] nameBytes = new byte[nameSize];
-            buffer.get(nameBytes);
-            name = new String(nameBytes, "UTF-8");
-
-            sexSize = buffer.getInt();
-            byte[] sexBytes = new byte[sexSize];
-            buffer.get(sexBytes);
-            sex = new String(sexBytes, "UTF-8");
-
-            User user = new User();
-            user.setId(id);
-            user.setSex(sex);
-            user.setName(name);
-
-            return user;
-        } catch (Exception e){
-            throw new SerializationException("反序列化出错" + e);
+            user = mapper.readValue(data, User.class);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return user;
     }
 
     @Override
